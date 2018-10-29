dc1afdcbc8becfcdc92ee5aa1b11ffaab8ba792a
John Haney <geojwh@gmail.com>
2018-10-29 09:48:33 -0500

    clean up script and document

--- image_work.py
+++ image_work.py
@@ -8,6 +8,7 @@ copies each image and renames the copy to a new uuid.
     - image_id = new uuid generated from get_s3_images function
     - collection_id = collection_id
     - image_url = new url
+   
    this function also replaces the existing collection table thumbnail_image records with the new uuid names.
 '''
 import os
@@ -16,6 +17,7 @@ import psycopg2
 import datetime
 
 # s3 variables used in all three functions
+# enter your own image names in the old_names list
 old_names = ['overview.jpg', 'urban.jpg', 'thumbnail.jpg', 'natural.jpg']
 client = boto3.client('s3')
 
@@ -78,29 +80,36 @@ def update_db(bucket, suffix=''):
     password = os.environ.get('DB_PASSWORD')
     host = os.environ.get('DB_HOST')
     port = os.environ.get('DB_PORT')
+    
     # connection string
     conn_string = "dbname='%s' user='%s' host='%s' password='%s' port='%s'" % (database, username, host, password, port)
+    
     # db table names
     collection_table = 'collection'
     image_table = 'image'
+    
     # s3 stuff to be used later
     kwargs = {'Bucket': bucket}
     base_url = 'https://s3.amazonaws.com/data.tnris.org/'
+    
     # connect to the database
     conn = psycopg2.connect(conn_string)
     cur = conn.cursor()
+    
     # sql statement to get both collection_id and thumbnail_image fields
     collection_query = "SELECT collection_id, thumbnail_image FROM %s;" % collection_table
     image_query = "SELECT image_id, image_url, collection_id FROM %s;" % image_table
+    
     # execute sql statements
     # cur.execute(collection_query)
     cur.execute(image_query)
+    
     # get response from both collection and image table queries
     db_response = cur.fetchall()
 
-    add_images = []
+    image_errors = []
     unique_cols = []
-    # print(db_response)
+    
     while True:
         # s3 stuff
         s3_resp = client.list_objects_v2(**kwargs)
@@ -113,26 +122,29 @@ def update_db(bucket, suffix=''):
                 img_url = base_url + key.strip()
                 col_id = uuid.UUID(key.split('/')[0].strip())
                 timestamp = datetime.datetime.now()
-
-                # print("inserting image {}".format(count))
-                # cur.execute("INSERT INTO {table} ({id},{url},{create},{modify},{col}) VALUES ('{v1}','{v2}','{v3}','{v4}','{v5}');".format(
-                #     table=image_table,
-                #     id='image_id',
-                #     url='image_url',
-                #     create='created',
-                #     modify='last_modified',
-                #     col='collection_id',
-                #     v1=img_id,
-                #     v2=img_url,
-                #     v3=timestamp,
-                #     v4=timestamp,
-                #     v5=col_id)
-                # )
-                # try:
-                #     conn.commit()
-                # except:
-                #     add_images.append(col_id)
-
+                
+                # insert values into the rds/postgres image table
+                print("inserting image {}".format(count))
+                cur.execute("INSERT INTO {table} ({id},{url},{create},{modify},{col}) VALUES ('{v1}','{v2}','{v3}','{v4}','{v5}');".format(
+                    table=image_table,
+                    id='image_id',
+                    url='image_url',
+                    create='created',
+                    modify='last_modified',
+                    col='collection_id',
+                    v1=img_id,
+                    v2=img_url,
+                    v3=timestamp,
+                    v4=timestamp,
+                    v5=col_id)
+                )
+                try:
+                    conn.commit()
+                except:
+                    # add the column id of any records with errors to the image_errors list
+                    image_errors.append(col_id)
+                
+                # update the collection table with the new s3 image url and uuid
                 cur.execute("UPDATE {table} SET {field} = '{url}' WHERE collection_id = '{col_id}';".format(
                     table=collection_table,
                     field='thumbnail_image',
@@ -143,18 +155,8 @@ def update_db(bucket, suffix=''):
                 try:
                     conn.commit()
                 except:
-                    add_images.append(col_id)
-
-                # cur.execute("INSERT INTO {table};".format(table=image_table))
-                # if col_id not in unique_cols:
-                #     unique_cols.append(col_id)
-                # if col_id not in unique_cols:
-                    # unique_cols.append(id)
-
-                # img_id = key.split('/')[-1].rstrip(suffix).strip()
-                # img_url = base_url + key.strip()
-                # add_images = add_images.append([img_id, img_url, col_id])
-                # cur.execute("INSERT INTO {table} ({id},{url},{col}) VALUES ({v_id}, {v_url}, {v_col});".format(table=image_table, id=image_id, url=image_url, col=collection_id, v_id=img_id, v_url=img_url, v_col=col_id)
+                    # add the collection id of any records with errors to the image_errors list
+                    image_errors.append(col_id)
 
         try:
             kwargs['ContinuationToken'] = s3_resp['NextContinuationToken']
@@ -162,29 +164,12 @@ def update_db(bucket, suffix=''):
             break
 
     print('Bad collections...')
-    print(add_images)
-
-    # for item in db_response:
-    #     image_id, image_url, collection_id = item
-    #     print(image_url)
-    #     cur.execute("INSERT INTO {table} ({id},{url},{col}) VALUES ({v_id}, {v_url}, {v_col});".format(table=image_table, id=image_id, url=image_url, col=collection_id, v_id=img_id, v_url=img_url, v_col=col_id)
+    print(image_errors)
 
 
-    # print(str(count) + ": " + col_id + ",\n" + img_id + ",\n" + img_url + ",\n")
-
-    # db stuff
-    # for item in db_response:
-    #     image_id, image_url, collection_id = item
-    #     print('|', image_id, '|', image_url, '|', collection_id, '|')
-
-    # if thumb.find('thumbnail.jpg') and id == collection:
-    #     update_thumb = "UPDATE %s SET thumbnail_image = %s;" % tablename, thumb
-    #     print(str(count) + ': ' + str(id) + ' == ' + str(collection) + '; updating with ' + str(image))
-    #     cur.execute(update_thumb)
-
-
-# execute functions
+# uncomment each function call below to execute each method one by one or all at the same time
 # get_s3_images('data.tnris.org', '.jpg')
+
 # delete_old('data.tnris.org', '.jpg')
 
-update_db('data.tnris.org', '.jpg')
+# update_db('data.tnris.org', '.jpg')
